e58d2f0afdd2ea920ad804a57d1118005d354215
Ander <ander@LAPTOP-5E1DCS3I.localdomain>
2023-07-27 16:40:04 +0200

    some refactoring

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# TNF
+# Terse Normal Form
+
+
+## Benchmark format 🛠️
+
+```
+Initial Formula
+formula
+
+Safety Formula
+formula1
+formula2
+  ... 
+
+```
+
+## Benchmark format 🚀
+
+```
+Initial Formula
+s
+
+Safety Formula
+p_e & s_e -> l
+
+```
+
+### Run script format 📋
+
+```
+python3 main.py benchmarkFile.txt
+
+```
--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from tools import read_benchmark_file
 def execute():
 
     initial_formula, safety_formula, _ = read_benchmark_file(sys.argv[1])
-    formula_str = " && ".join(initial_formula + safety_formula)
+    initial_formula_str = f'{" && ".join(initial_formula)}'
+    safety_formula_str = f'{" && ".join(safety_formula)}'
+
+    formula_str = f"({initial_formula_str}) && ({safety_formula_str})"
     TNF(formula_str, info=dict(), activated_apply_subsumptions=False, activated_print_info=False, activated_print_tnf=True, activate_verification=False)
 
 execute()
